201c1ed1b8c08ee8616bb822894e8e7630d6cd42
raulgp <raul.garrigos@hotmail.com>
2023-11-30 16:46:07 +0100

    Added context to Nav and logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,36 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
 
 function Navbar() {
-  return (
-    <div>
-      <Link to="/">Home</Link>
-      <span> | </span>
-      <Link to="/register">Create user</Link>
-      <span> | </span>
-      <Link to="/login">Login</Link>
-    </div>
-  );
+  const { isLoggedIn, authenticateUser } = useContext(AuthContext);
+
+  const redirect = useNavigate();
+
+  const handleLogOut = () => {
+    localStorage.removeItem("authToken");
+
+    authenticateUser();
+
+    redirect("/");
+  };
+
+  if (isLoggedIn) {
+    return (
+      <nav>
+        <Link to="/">Home</Link>
+        <button onClick={handleLogOut}>Log Out</button>
+      </nav>
+    );
+  } else {
+    return (
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/register">Create Account</Link>
+        <Link to="/login">Log In</Link>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
