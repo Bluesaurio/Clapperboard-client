fa799fb928779a1ef8ce985e65cd05be161bc4f9
raulgp <raul.garrigos@hotmail.com>
2023-12-03 18:36:02 +0100

    Called BackEnd route to get popular movies in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import service from "../services/config";
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div>Home</div>;
+  const [popularMovies, setPopularMovies] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await service.get("/movie/popular");
+      console.log(response.data.results);
+      setPopularMovies(response.data.results);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (isLoading) {
+    return <h3>Buscando data</h3>;
+  }
+
+  return (
+    <div>
+      {popularMovies.map((eachMovie) => {
+        return (
+          <Link to={`movie/${eachMovie.id}`} key={eachMovie.id}>
+            <p>{eachMovie.original_title}</p>
+            <img
+              src={`https://www.themoviedb.org/t/p/w200/${eachMovie.poster_path}`}
+            ></img>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Home;
