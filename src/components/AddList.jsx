6ceb203a97e0fa3aa28be18e4342b2dca70fac11
raulgp <raul.garrigos@hotmail.com>
2023-12-06 12:22:18 +0100

    Added list of lists and list form

--- src/components/AddList.jsx
+++ src/components/AddList.jsx
@@ -1,5 +1,75 @@
+import { useState } from "react";
+import service from "../services/config";
+import { useNavigate, useParams } from "react-router-dom";
+
 function AddList() {
-  return <div>AddList</div>;
+  const params = useParams();
+  console.log(params.userId);
+  const redirect = useNavigate();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("Custom");
+
+  const handleListName = (e) => {
+    setName(e.target.value);
+  };
+  const handleDescription = (e) => {
+    setDescription(e.target.value);
+  };
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const customList = { name, description, category };
+    console.log(customList);
+
+    try {
+      const response = await service.post(
+        `/profile/${params.userId}/lists`,
+        customList
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      redirect("/error");
+    }
+  };
+
+  return (
+    <div>
+      <h4>Create your lists</h4>
+      <form onSubmit={handleSubmit}>
+        <label>Give a name to your list: </label>
+        <br />
+
+        <input type="text" value={name} onChange={handleListName} />
+        <br />
+        <br />
+
+        <label>What's your list about?: </label>
+        <br />
+
+        <textarea value={description} onChange={handleDescription}></textarea>
+        <br />
+        <br />
+
+        <label>What type of list is it?: </label>
+        <br />
+
+        <select value={category} onChange={handleCategory}>
+          <option value="Custom">Custom</option>
+          <option value="Favorites">Favorites</option>
+          <option value="Watchlist">Watchlist</option>
+        </select>
+        <br />
+        <br />
+
+        <button type="submit">Create it</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddList;
--- src/pages/list/CustomLists.jsx
+++ src/pages/list/CustomLists.jsx
@@ -1,5 +1,51 @@
+import { Link, useNavigate, useParams } from "react-router-dom";
+import AddList from "../../components/AddList";
+import { useEffect, useState } from "react";
+import service from "../../services/config";
+
 function CustomList() {
-  return <div>CustomList</div>;
+  const params = useParams();
+  const redirect = useNavigate();
+
+  const [allUserLists, setAllUserLists] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await service.get(`/profile/${params.userId}/lists`);
+      console.log(response.data);
+      setAllUserLists(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      redirect("/error");
+    }
+  };
+
+  if (isLoading) {
+    return <h3>Searching</h3>;
+  }
+
+  return (
+    <div>
+      {allUserLists.map((eachList) => {
+        return (
+          <div key={eachList._id}>
+            <p>Name: {eachList.name}</p>
+            <p>Description: {eachList.description}</p>
+            <Link to={`/profile/${params.userId}/lists/details`}>
+              <p>Imaginemos que esto es una imagen</p>
+            </Link>
+          </div>
+        );
+      })}
+      <AddList />
+    </div>
+  );
 }
 
 export default CustomList;
