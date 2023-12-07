22ca83ac800768e0371af982cd907a086986fe1e
Bluesaurio <adriansiquiervalles@gmail.com>
2023-12-07 20:33:34 +0100

    custom list new div added

--- src/pages/list/CustomLists.jsx
+++ src/pages/list/CustomLists.jsx
@@ -59,39 +59,45 @@ function CustomList() {
             <Link
               to={`/profile/${params.userId}/lists/${eachList._id}/details`}
             >
-              <div className="image-container">
-                {eachList.filmDetails && eachList.filmDetails[0] && (
-                  <ImageApi
-                    path={eachList.filmDetails[0].image}
-                    alt={eachList.filmDetails[0].title}
-                    className="list-image-grid"
-                  />
-                )}
-                {eachList.filmDetails && eachList.filmDetails[1] && (
-                  <ImageApi
-                    path={eachList.filmDetails[1].image}
-                    alt={eachList.filmDetails[1].title}
-                    className="list-image-grid"
-                  />
-                )}
-                {eachList.filmDetails && eachList.filmDetails[2] && (
-                  <ImageApi
-                    path={eachList.filmDetails[2].image}
-                    alt={eachList.filmDetails[2].title}
-                    className="list-image-grid"
-                  />
-                )}
-                {eachList.filmDetails && eachList.filmDetails[3] && (
-                  <ImageApi
-                    path={eachList.filmDetails[3].image}
-                    alt={eachList.filmDetails[3].title}
-                    className="list-image-grid"
-                  />
-                )}
-
-                {loggedUser?._id === params.userId && !eachList.filmDetails && (
-                  <p className="add-movie-text">Click here to add a movie!</p>
-                )}
+              <div>
+                <div className="image-container">
+                  {eachList.filmDetails && eachList.filmDetails[0] && (
+                    <ImageApi
+                      path={eachList.filmDetails[0].image}
+                      alt={eachList.filmDetails[0].title}
+                      className="list-image-grid"
+                    />
+                  )}
+                  {eachList.filmDetails && eachList.filmDetails[1] && (
+                    <ImageApi
+                      path={eachList.filmDetails[1].image}
+                      alt={eachList.filmDetails[1].title}
+                      className="list-image-grid"
+                    />
+                  )}
+                  {eachList.filmDetails && eachList.filmDetails[2] && (
+                    <ImageApi
+                      path={eachList.filmDetails[2].image}
+                      alt={eachList.filmDetails[2].title}
+                      className="list-image-grid"
+                    />
+                  )}
+                  {eachList.filmDetails && eachList.filmDetails[3] && (
+                    <ImageApi
+                      path={eachList.filmDetails[3].image}
+                      alt={eachList.filmDetails[3].title}
+                      className="list-image-grid"
+                    />
+                  )}
+                </div>
+                <div>
+                  {loggedUser?._id === params.userId &&
+                    !eachList.filmDetails && (
+                      <p className="add-movie-text">
+                        Click here to add a movie!
+                      </p>
+                    )}
+                </div>
               </div>
             </Link>
           </div>
